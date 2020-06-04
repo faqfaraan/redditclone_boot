ec63f195e2e6a120f3e97adeb42f4fa78ffea67d
Faraan <faqfaraan@gmail.com>
2020-06-04 15:40:12 -0400

    first commit

---
+++ src/test/java/faq/redditclone/RedditcloneApplicationTests.java
@@ -0,0 +1,13 @@
+package faq.redditclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditcloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
